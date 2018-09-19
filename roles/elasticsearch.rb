13423017b22d1f0afb2f63d903bd48cb529d5751
Giovanni Sakti <giosakti@gmail.com>
2018-09-19 18:35:49 +0700

    update elasticsearch cookbook

--- roles/elasticsearch.rb
+++ roles/elasticsearch.rb
@@ -1,3 +1,3 @@
 name "elasticsearch"
 description "Elasticsearch"
-run_list "recipe[consul]", "recipe[elasticsearch_wrapper_cookbook]", "recipe[elasticsearch_wrapper_cookbook::consul_register]"
+run_list "recipe[consul]", "recipe[elasticsearch_wrapper_cookbook::elasticsearch]", "recipe[elasticsearch_wrapper_cookbook::consul_register]"
