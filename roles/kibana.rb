5d6e339c4ce8bca3e8b03d0839c11b3b62c125bd
Fadli Nurhasan <fadli.nurhasan@gmail.com>
2020-06-19 22:59:20 +0700

    remove elastalert from kibana role

--- roles/kibana.rb
+++ roles/kibana.rb
@@ -1,3 +1,3 @@
 name "kibana"
 description "Kibana"
-run_list "recipe[consul]", "recipe[kibana]", "recipe[kibana::consul_register]", "recipe[kibana::elastalert]", "recipe[prometheus::kibana_exporter]"
+run_list "recipe[consul]", "recipe[kibana]", "recipe[kibana::consul_register]", "recipe[prometheus::kibana_exporter]"
