393b428f7d57417daaa163dba1363a37bf4c7561
Galuh Estya <galuhestya@gmail.com>
2020-08-03 10:00:37 +0700

    call ES indices operation after consul registration

--- roles/elasticsearch.rb
+++ roles/elasticsearch.rb
@@ -1,3 +1,3 @@
 name "elasticsearch"
 description "Elasticsearch"
-run_list "recipe[consul]", "recipe[elasticsearch_wrapper_cookbook::elasticsearch]", "recipe[elasticsearch_wrapper_cookbook::consul_register]", "recipe[prometheus::elasticsearch_exporter]"
+run_list "recipe[consul]", "recipe[elasticsearch_wrapper_cookbook::elasticsearch]", "recipe[elasticsearch_wrapper_cookbook::consul_register]", "recipe[elasticsearch_wrapper_cookbook::elasticsearch_api]", "recipe[prometheus::elasticsearch_exporter]"
