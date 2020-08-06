e85a8f0f87c8895ad9aa42dd58c0b48b2db7759c
Galuh Estya <galuhestya@gmail.com>
2020-08-06 15:11:38 +0700

    Readd "Merge pull request #24 from BaritoLog/elastic-7.5""
    This reverts commit 9261c97cf3e1a9b9aff224299bb4e179405dd3bc.

--- roles/barito-flow-producer.rb
+++ roles/barito-flow-producer.rb
@@ -1,3 +1,3 @@
 name "barito-flow-producer"
 description "Barito Flow Producer"
-run_list "recipe[consul]", "recipe[barito-flow::producer_consul_register]", "recipe[barito-flow::producer]"
+run_list "recipe[consul]", "recipe[barito-flow::producer]", "recipe[barito-flow::producer_consul_register]"
