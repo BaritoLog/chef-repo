5480279556517db91d0737d5f6b54f49836b75bf
Eka Risky F (ekarisky) <ekarisky@gmail.com>
2021-01-13 14:55:09 +0700

    reorder consul role from barito-flow-consumer
    Co-authored-by: Fadli Nurhasan <fadli.nurhasan@gmail.com>

--- roles/barito-flow-consumer.rb
+++ roles/barito-flow-consumer.rb
@@ -1,3 +1,3 @@
 name "barito-flow-consumer"
 description "Barito Flow Consumer"
-run_list "recipe[consul]", "recipe[barito-flow::consumer]", "recipe[barito-flow::consumer_consul_register]"
+run_list "recipe[barito-flow::consumer]", "recipe[consul]", "recipe[barito-flow::consumer_consul_register]"
